8845a7697af0ea018aacbe5eac2a65a9c62b4917
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-08-17 16:01:54 +0300

    created main folders and files

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ const express = require('express');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
---
+++ errors/handleError.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+module.exports = (err, req, res, next) => {
+  if (!err.statusCode) {
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
+  }
+  res.status(err.statusCode).send({ message: err.message });
+};
