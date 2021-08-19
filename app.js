c0af152537f435e08ccd49c7f900f5bd9fa61a93
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-08-19 12:56:21 +0300

    create index.js for all routes

--- app.js
+++ app.js
@@ -6,14 +6,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const { SETUP_MONGO, URL_MONGO } = require('./utils/constants');
-const NotFoundError = require('./errors/NotFoundError');
 const handleError = require('./errors/handleError');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/limiter');
-const { urlUnkownError } = require('./errors/messages');
-const { signinValidation, signupValidation } = require('./middlewares/validation');
+const router = require('./routes/index');
 
 const { PORT = 3003 } = process.env;
 const app = express();
@@ -26,19 +22,7 @@ app.use(requestLogger);
 app.use(helmet());
 app.use(cors());
 app.use(limiter);
-
-app.post('/signin', signinValidation, login);
-app.post('/signup', signupValidation, createUser);
-
-app.use(auth);
-
-app.use('/users', require('./routes/users'));
-app.use('/movies', require('./routes/movies'));
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundError(urlUnkownError.urlUnkown));
-});
-
+app.use(router);
 app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
